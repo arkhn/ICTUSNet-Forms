e20cd42c97cb48f90074e223bf9eaff724b5e2b5
Teva RIOU <teva.riou@gmail.com>
2020-11-25 18:53:16 +0100

    Link user and patients

--- api/avc_forms/api/models.py
+++ api/avc_forms/api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Patient(models.Model):
     code = models.TextField(default='', unique=True)
     data = models.JSONField()
+    owner = models.ForeignKey('auth.User', related_name='patients', null=True, on_delete=models.SET_NULL)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
