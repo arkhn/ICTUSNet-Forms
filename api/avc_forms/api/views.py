b6d3e4caa99323315d38d1c6fc2639f512ed27b7
Teva RIOU <teva.riou@gmail.com>
2021-01-26 13:26:27 +0100

    fix: remove user view

--- api/avc_forms/api/views.py
+++ api/avc_forms/api/views.py
@@ -1,17 +1,7 @@
-from django.contrib.auth.models import User
 from avc_forms.api.models import Patient
 from rest_framework import viewsets
 from rest_framework import permissions
-from avc_forms.api.serializers import UserSerializer, PatientSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+from avc_forms.api.serializers import PatientSerializer
 
 
 class PatientViewSet(viewsets.ModelViewSet):
